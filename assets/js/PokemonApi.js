d742a5329cd2acf2f2d212905611c19cb09e2c89
nakashiy <whqwd667@gmail.com>
2022-04-15 22:50:29 +0900

    表示項目追加

--- assets/js/PokemonApi.js
+++ assets/js/PokemonApi.js
@@ -1,5 +1,6 @@
 // 定数設定
 const BASE_URL = 'https://pokeapi.co/api/v2/';
+const BASE_IMG_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 const URL_PARAM = [
     'pokemon', //基本情報
     'pokemon-species' //種族情報
@@ -20,6 +21,14 @@ class PokemonApi {
             const data_json = await data.json();
             this.info[param] = data_json;
         }
-        this.info['pokemon-img'] = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + number + '.png';
+        this.info['pokemon-img'] = {};
+        this.info['pokemon-img']['front'] = BASE_IMG_URL + number + '.png';
+        this.info['pokemon-img']['front-shiny'] = BASE_IMG_URL + 'shiny/' + number + '.png';
+        this.info['pokemon-img']['front-female'] = BASE_IMG_URL + 'female/' + number + '.png';
+        this.info['pokemon-img']['front-shiny-female'] = BASE_IMG_URL + 'shiny/female/' + number + '.png';
+        this.info['pokemon-img']['back'] = BASE_IMG_URL + 'back/' + number + '.png';
+        this.info['pokemon-img']['back-shiny'] = BASE_IMG_URL + 'back/shiny/' + number + '.png';
+        this.info['pokemon-img']['back-female'] = BASE_IMG_URL + 'back/female/' + number + '.png';
+        this.info['pokemon-img']['back-shiny-female'] = BASE_IMG_URL + 'back/shiny/female/' + number + '.png';
     };
 }
--- index.php
+++ index.php
@@ -49,15 +49,26 @@
     <!-- JSコンテンツstart -->
     <button type="button" class="btn btn-danger" onclick="displayPokemon()">ポケモン！</button>
     <div id="pokemon_wrap">
-        <img id="pokemon_img"></img>
-        <div id="pokemon_id"></div>
-        <div id="pokemon_name"></div>
+        <img id="img_front"></img>
+        <div id="info">
+            <div>　　ＩＤ：<span id="id"></span></div>
+            <div>　　名前：<span id="name"></span></div>
+            <div>　　ＨＰ：<span id="hp"></span></div>
+            <div>こうげき：<span id="attack"></span></div>
+            <div>ぼうぎょ：<span id="defense"></span></div>
+            <div>とくこう：<span id="special_attack"></span></div>
+            <div>とくぼう：<span id="special_defense"></span></div>
+            <div>すばやさ：<span id="speed"></span></div>
+            <div>　　合計：<span id="total"></span></div>
+        </div>
     </div>
     <!-- JSコンテンツend -->
 
     <script>
         // ページが全て読み込まれたら実行
-        window.onload = function() {};
+        window.onload = function() {
+            displayPokemon();
+        };
         // ポケモンを表示
         const displayPokemon = async () => {
             // ポケモン情報取得
@@ -65,13 +76,32 @@
             const pokemonApi = new PokemonApi();
             await pokemonApi.init(No);
             console.log(pokemonApi.info);
+            //ステータスの合計算出
+            let total_stat = 0;
+            for (let i = 0; i < 6; i++) {
+                total_stat += pokemonApi.info['pokemon']['stats'][i]['base_stat'];
+            }
             // ポケモン情報の表示
-            const pokemon_img = document.getElementById('pokemon_img');
-            pokemon_img.src = pokemonApi.info['pokemon-img'];
-            const pokemon_id = document.getElementById('pokemon_id');
-            pokemon_id.innerHTML = pokemonApi.info['pokemon']['id'];
-            const pokemon_name = document.getElementById('pokemon_name');
-            pokemon_name.innerHTML = pokemonApi.info['pokemon-species']['names'][0]['name'];
+            const img_front = document.getElementById('img_front');
+            img_front.src = pokemonApi.info['pokemon-img']['front'];
+            const id = document.getElementById('id');
+            id.innerHTML = pokemonApi.info['pokemon']['id'];
+            const name = document.getElementById('name');
+            name.innerHTML = pokemonApi.info['pokemon-species']['names'][0]['name'];
+            const hp = document.getElementById('hp');
+            hp.innerHTML = pokemonApi.info['pokemon']['stats'][0]['base_stat'];
+            const attack = document.getElementById('attack');
+            attack.innerHTML = pokemonApi.info['pokemon']['stats'][1]['base_stat'];
+            const defense = document.getElementById('defense');
+            defense.innerHTML = pokemonApi.info['pokemon']['stats'][2]['base_stat'];
+            const special_attack = document.getElementById('special_attack');
+            special_attack.innerHTML = pokemonApi.info['pokemon']['stats'][3]['base_stat'];
+            const special_defense = document.getElementById('special_defense');
+            special_defense.innerHTML = pokemonApi.info['pokemon']['stats'][4]['base_stat'];
+            const speed = document.getElementById('speed');
+            speed.innerHTML = pokemonApi.info['pokemon']['stats'][5]['base_stat'];
+            const total = document.getElementById('total');
+            total.innerHTML = total_stat;
         };
     </script>
     <!-- JSライブラリ -->
